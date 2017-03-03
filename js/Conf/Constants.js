7ca5f74f3ce1273c71249c518f94c6d36f2089b3
ammoradi <amirmohammad.moradi@gmail.com>
2017-03-04 00:33:08 +0330

    server address corrected

--- js/Conf/Constants.js
+++ js/Conf/Constants.js
@@ -1,7 +1,7 @@
 ijust.constant("Constants", {
-    // server : "https://ijust.ir/" ,
+    server : "https://ijust.ir/" ,
     version :"v1/",
-    server : "http://test.ijust.ir:8080/api/" ,
+    // server : "http://test.ijust.ir:8080/api/" ,
     // server : "http://172.17.8.120:8080/api/" ,
 
     errorMessages: {
