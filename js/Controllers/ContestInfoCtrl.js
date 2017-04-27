25e2dd43ca05524005f4e0bcc53bf71b8fe271d1
ammoradi <amirmohammad.moradi@gmail.com>
2017-04-27 19:32:05 +0430

    timer added to contest

--- js/Controllers/ContestInfoCtrl.js
+++ js/Controllers/ContestInfoCtrl.js
@@ -1,4 +1,4 @@
-var ContestInfoCtrl = function ($scope, mtNotifyService, $stateParams,
+var ContestInfoCtrl = function ($scope, mtNotifyService, $stateParams, $interval,
                                 ContestModel, TeamModel, $rootScope , $localStorage) {
 
     $rootScope.notifyLoader = true;
@@ -27,7 +27,45 @@ var ContestInfoCtrl = function ($scope, mtNotifyService, $stateParams,
                 $rootScope.myTeam = data.joining_status.team;
                 $rootScope.notifyLoader = false;
             }
+            $rootScope.timer = {
+                time : "",
+                status : ""
+            };
         }
+    });
+
+    function makeTime(t){
+        var hours   = Math.floor(t / 3600);
+        var minutes = Math.floor((t - (hours * 3600)) / 60);
+        var seconds = t - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        $rootScope.timer.time = hours+':'+minutes+':'+seconds;
+    }
+
+    // countdown Interval
+    $scope.myInterval = $interval(function () {
+        var now = Date.parse(new Date())/1000 ;
+        if($rootScope.contestInfo.starts_at > now){
+            var t = $rootScope.contestInfo.starts_at - now;
+            makeTime(t);
+            $rootScope.timer.status = 'to start' ;
+        }
+        else if (($rootScope.contestInfo.starts_at <= now)&&(now < $rootScope.contestInfo.ends_at)){
+            var tt = $rootScope.contestInfo.ends_at - now;
+            makeTime(tt);
+            $rootScope.timer.status = 'to end' ;
+        }
+        else {
+            $rootScope.timer.status += 'ended' ;
+        }
+    }, 1000);
+
+    // to Destroy Interval
+    $scope.$on('$destroy', function () {
+        $interval.cancel($scope.myInterval);
     });
 
     $scope.join = function (teamId) {
