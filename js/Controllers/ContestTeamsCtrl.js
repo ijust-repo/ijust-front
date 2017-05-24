52476a4a9f0f8b68219f3e30f697a32c69116a58
ammoradi <amirmohammad.moradi@gmail.com>
2017-05-25 02:33:03 +0430

    some bugs fixed

--- js/Controllers/ContestTeamsCtrl.js
+++ js/Controllers/ContestTeamsCtrl.js
@@ -1,12 +1,13 @@
 var ContestTeamsCtrl = function ($scope , $rootScope,ContestModel) {
     $rootScope.notifyLoader = true ;
     $scope.joinedTeams = {} ;
-
+    $scope.joinedCnt = 0 ;
     try {
         ContestModel.getAcceptedTeams($rootScope.contestId,function (data , status) {
             if (status){
                 $scope.joinedTeams = data.accepted_teams;
                 $scope.joinedTeamsNumber = $scope.joinedTeams.length;
+                $scope.joinedCnt = $scope.joinedTeams.length;
                 $rootScope.notifyLoader = false ;
             }
         });
